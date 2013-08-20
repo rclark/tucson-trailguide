a113fa7ac18b79ca45ca6ab8f02dd243e7fe1467
Ryan Clark <ryan.clark.j@gmail.com>
2013-08-19 17:48:53 -0700

    continuing to learn about travis post body and express.js interactions

--- updates.js
+++ updates.js
@@ -4,7 +4,7 @@ var express = require('express'),
     app = express(),
     showMessages = true;
 
-//app.use(express.bodyParser());
+app.use(express.bodyParser());
 
 app.post('/update-hook', function (req, res) {
     if (req.header('Authorization') !== config.updates.auth) {
