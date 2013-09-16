7001dda540f821f58438b0acabba39b5581b42d3
David Clark <david.dave.clark@gmail.com>
2013-09-15 17:52:57 -0700

    Added empty methods with comments to segment model.

--- src/js/models/segment.js
+++ src/js/models/segment.js
@@ -26,23 +26,34 @@ trailguide.models.Segment = Backbone.Model.extend({
     };
   },
 
-  getDistance: function() {
+  getDistance: function () {
     return this.get('jstsGeometry').getLength();
   },
 
-  getEndpoints: function() {
+  getEndpoints: function () {
     var coords = this.get('geometry').coordinates;
     endpoints = [coords[0], coords[coords.length - 1]];
     return endpoints;
   },
 
-  getAdjacentTrailheads: function() {
-
+  getAdjacentTrailheads: function () {
+    // get any trailheads corresponding with
+    // this segment's endpoints
   },
 
-  getAdjacentSegments: function() {
-    var url = 'db/segments/_design/geo/_view/endpoints?keys=' + this.getEndpoints() + '';
+  getAdjacentSegments: function () {
+    // get any other segments corresponding with
+    // this segment's endpoints
+    var url = 'db/segments/_design/geo/_view/endpoints?keys=' + this.getEndpoints();
+  },
 
+  getPOIs: function() {
+    // get POIs associated with this segment
+    var poiIDs = this.get('pois');
+  },
+
+  getRelatedRoutes: function () {
+    // get any routes that include this segment
   },
 
   leafletLayer: function (options) {
@@ -50,17 +61,19 @@ trailguide.models.Segment = Backbone.Model.extend({
     return L.geoJson(this.toJSON(), options);
   },
 
-  details: function() {
+  details: function () {
+    // prepare the details object for
+    // this segment's view
     var details = {
       'distance': this.getDistance()
     };
 
-    var pois = this.get('pois') || false;
+    var pois = this.get('pois');
     if (pois) {
       details.pois = pois;
     }
 
-    var accessibility = this.get('accessibility') || false;
+    var accessibility = this.get('accessibility');
     if (accessibility) {
       details.accessibility = accessibility;
     }
